2343c2b568fdc98d36faca1011011760d4871787
navib <hillpote@gamil.com>
2026-03-05 13:01:45 +0800

    feat(spatial.generators): 添加新的几何体生成器节点
    - 注册椭球体生成器节点 (Ellipsoid Blocks)
    - 注册圆锥体生成器节点 (Cone Blocks)
    - 注册圆环生成器节点 (Torus Blocks)
    - 注册正八面体生成器节点 (Octahedron Blocks)
    - 注册正四面体生成器节点 (Tetrahedron Blocks)
    - 注册三棱锥生成器节点 (Triangular Pyramid Blocks)
    - 注册三棱柱生成器节点 (Triangular Prism Blocks)

--- src/main/java/com/nodecraft/nodesystem/nodes/spatial/generators/SpatialGeneratorNodes.java
+++ src/main/java/com/nodecraft/nodesystem/nodes/spatial/generators/SpatialGeneratorNodes.java
@@ -22,5 +22,14 @@ public class SpatialGeneratorNodes {
         registry.registerNode(new NodeInfo("spatial.generators.cylinderblocks", "Cylinder Blocks", "", "spatial.generators", CylinderBlocksNode.class));
         registry.registerNode(new NodeInfo("spatial.generators.polylineblocks", "Polyline Blocks", "", "spatial.generators", PolylineBlocksNode.class));
         registry.registerNode(new NodeInfo("spatial.generators.curveblocks", "Curve Blocks", "", "spatial.generators", CurveBlocksNode.class));
+        
+        // 新增几何体生成器节点
+        registry.registerNode(new NodeInfo("spatial.generators.ellipsoidblocks", "Ellipsoid Blocks", "生成椭球体", "spatial.generators", EllipsoidBlocksNode.class));
+        registry.registerNode(new NodeInfo("spatial.generators.coneblocks", "Cone Blocks", "生成圆锥体", "spatial.generators", ConeBlocksNode.class));
+        registry.registerNode(new NodeInfo("spatial.generators.torusblocks", "Torus Blocks", "生成圆环", "spatial.generators", TorusBlocksNode.class));
+        registry.registerNode(new NodeInfo("spatial.generators.octahedronblocks", "Octahedron Blocks", "生成正八面体", "spatial.generators", OctahedronBlocksNode.class));
+        registry.registerNode(new NodeInfo("spatial.generators.tetrahedronblocks", "Tetrahedron Blocks", "生成正四面体", "spatial.generators", TetrahedronBlocksNode.class));
+        registry.registerNode(new NodeInfo("spatial.generators.triangularpyramidblocks", "Triangular Pyramid Blocks", "生成三棱锥", "spatial.generators", TriangularPyramidBlocksNode.class));
+        registry.registerNode(new NodeInfo("spatial.generators.triangularprismblocks", "Triangular Prism Blocks", "生成三棱柱", "spatial.generators", TriangularPrismBlocksNode.class));
     }
 } 
