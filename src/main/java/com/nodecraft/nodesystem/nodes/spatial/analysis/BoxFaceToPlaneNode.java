f16cece4a9b860d50a8624cefa4dc70a047a0b8d
OOPS <hillpote@gmail.com>
2026-04-05 21:18:17 +0800

    feat(spatial): 添加盒面转平面节点功能
    - 实现了BoxFaceToPlaneNode类，用于将盒面转换为支撑平面
    - 定义了输入端口接收盒面数据
    - 创建了多个输出端口输出平面、中心点、法向量、名称、索引和有效性信息
    - 实现了节点处理逻辑，支持盒面数据到平面数据的转换
    - 集成了执行上下文处理和数据验证功能

---
+++ src/main/java/com/nodecraft/nodesystem/nodes/spatial/analysis/BoxFaceToPlaneNode.java
@@ -0,0 +1,69 @@
+package com.nodecraft.nodesystem.nodes.spatial.analysis;
+
+import com.nodecraft.nodesystem.api.NodeDataType;
+import com.nodecraft.nodesystem.api.NodeInfo;
+import com.nodecraft.nodesystem.core.BaseNode;
+import com.nodecraft.nodesystem.core.BasePort;
+import com.nodecraft.nodesystem.datatypes.BoxFaceData;
+import com.nodecraft.nodesystem.execution.ExecutionContext;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+@NodeInfo(
+    id = "spatial.analysis.box_face_to_plane",
+    displayName = "Box Face To Plane",
+    description = "Converts a box face into its supporting plane and related face frame data",
+    category = "spatial.analysis"
+)
+public class BoxFaceToPlaneNode extends BaseNode {
+
+    private static final String INPUT_FACE_ID = "input_face";
+
+    private static final String OUTPUT_PLANE_ID = "output_plane";
+    private static final String OUTPUT_CENTER_ID = "output_center";
+    private static final String OUTPUT_NORMAL_ID = "output_normal";
+    private static final String OUTPUT_NAME_ID = "output_name";
+    private static final String OUTPUT_INDEX_ID = "output_index";
+    private static final String OUTPUT_VALID_ID = "output_valid";
+
+    public BoxFaceToPlaneNode() {
+        super(UUID.randomUUID(), "spatial.analysis.box_face_to_plane");
+
+        addInputPort(new BasePort(INPUT_FACE_ID, "Face", "The box face to convert", NodeDataType.BOX_FACE, this));
+
+        addOutputPort(new BasePort(OUTPUT_PLANE_ID, "Plane", "Plane that contains the box face", NodeDataType.PLANE, this));
+        addOutputPort(new BasePort(OUTPUT_CENTER_ID, "Center", "Center point of the face", NodeDataType.VECTOR, this));
+        addOutputPort(new BasePort(OUTPUT_NORMAL_ID, "Normal", "Face normal vector", NodeDataType.VECTOR, this));
+        addOutputPort(new BasePort(OUTPUT_NAME_ID, "Name", "Face name", NodeDataType.STRING, this));
+        addOutputPort(new BasePort(OUTPUT_INDEX_ID, "Index", "Face index", NodeDataType.INTEGER, this));
+        addOutputPort(new BasePort(OUTPUT_VALID_ID, "Valid", "Whether a valid face was provided", NodeDataType.BOOLEAN, this));
+    }
+
+    @Override
+    public String getDescription() {
+        return "Converts a box face into its supporting plane and related face frame data";
+    }
+
+    @Override
+    public void processNode(@Nullable ExecutionContext context) {
+        Object faceObj = inputValues.get(INPUT_FACE_ID);
+
+        if (!(faceObj instanceof BoxFaceData face)) {
+            outputValues.put(OUTPUT_PLANE_ID, null);
+            outputValues.put(OUTPUT_CENTER_ID, null);
+            outputValues.put(OUTPUT_NORMAL_ID, null);
+            outputValues.put(OUTPUT_NAME_ID, null);
+            outputValues.put(OUTPUT_INDEX_ID, null);
+            outputValues.put(OUTPUT_VALID_ID, false);
+            return;
+        }
+
+        outputValues.put(OUTPUT_PLANE_ID, face.getPlane());
+        outputValues.put(OUTPUT_CENTER_ID, face.getCenter());
+        outputValues.put(OUTPUT_NORMAL_ID, face.getNormal());
+        outputValues.put(OUTPUT_NAME_ID, face.getName());
+        outputValues.put(OUTPUT_INDEX_ID, face.getIndex());
+        outputValues.put(OUTPUT_VALID_ID, true);
+    }
+}
